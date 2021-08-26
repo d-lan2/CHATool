27204d53e553028299c79922351600566a522130
dylan <dylan.riley@hotmail.com>
2021-08-26 13:12:09 +0100

    Cleanup and refactor

--- src/services/WordWriter.py
+++ src/services/WordWriter.py
@@ -6,14 +6,20 @@ from ..classes import Output
 class WordWriter:
     document = None
 
-    def write(self,results,filePath):
+    def write(self,results : Output.Result,filePath):
         self.cloneTemplate(filePath)
         self.writeHeaders(results)
         self.document.save(filePath)
     
     def writeHeaders(self,results: Output.Result):
         headerTable = self.document.tables[1]
-        for key in results.missingHeadersReportData:
+
+        allHeadersReportData = {}
+        allHeadersReportData.update(results.missingHeadersReportData)
+        allHeadersReportData.update(results.almostDeprecatedHeadersReportData)
+        allHeadersReportData.update(results.deprecatedHeadersReportData)
+
+        for key in allHeadersReportData:
             newTableRowCells = headerTable.add_row().cells
             newTableRowCells[0].text = key
             newTableRowCells[1].text = results.missingHeadersReportData[key]
@@ -30,46 +36,3 @@ class WordWriter:
              print("Cannot save doc to specified locaiton")
 
         self.document = document
-
-    def write1():
-        document = Document()
-
-        document.add_heading('Document Title', 0)
-
-        p = document.add_paragraph('A plain paragraph having some ')
-        p.add_run('bold').bold = True
-        p.add_run(' and some ')
-        p.add_run('italic.').italic = True
-
-        document.add_heading('Heading, level 1', level=1)
-        document.add_paragraph('Intense quote', style='Intense Quote')
-
-        document.add_paragraph(
-            'first item in unordered list', style='List Bullet'
-        )
-        document.add_paragraph(
-            'first item in ordered list', style='List Number'
-        )
-
-        #document.add_picture('monty-truth.png', width=Inches(1.25))
-
-        records = (
-            (3, '101', 'Spam'),
-            (7, '422', 'Eggs'),
-            (4, '631', 'Spam, spam, eggs, and spam')
-        )
-
-        table = document.add_table(rows=1, cols=3)
-        hdr_cells = table.rows[0].cells
-        hdr_cells[0].text = 'Qty'
-        hdr_cells[1].text = 'Id'
-        hdr_cells[2].text = 'Desc'
-        for qty, id, desc in records:
-            row_cells = table.add_row().cells
-            row_cells[0].text = str(qty)
-            row_cells[1].text = id
-            row_cells[2].text = desc
-
-        document.add_page_break()
-
-        document.save('demo.docx')
