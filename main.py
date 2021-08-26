878c24175c79a73cecf5c78028382e7bff046c23
dylan <dylan.riley@hotmail.com>
2021-08-26 14:01:03 +0100

    Move main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 """CHATool Cookie and Header Auditor Tool"""
 
 from requests.models import Response
-from classes.HTTP import HTTP
-from services.Auditor import Auditor
-from services.WordWriter import write
+from src.classes.HTTP import HTTP
+from src.services.Auditor import Auditor
+from src.services.WordWriter import WordWriter
 
 
 def main():
     print("Welcome to CHATool!")
     print("The Cookie Header Audior Tool")
-    write()
     url = input("Input url to scan the security headers:")
     response = HTTP.get(url)
-    Auditor.analyseHeaders(response)
+    results = Auditor().analyseHeaders(response)
+    WordWriter().writeDoc(results, "output\\test3.docx")
     print("Status code:" + str(response.status_code))
     input("Press enter to exit")
     
